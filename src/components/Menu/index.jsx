9115b4176f8b30efc4131db43bd2e896ede8ad04
Artem <artyom.kuroptev@yellow.systems>
2022-08-16 17:57:37 +0800

    Updated Menu with comment

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from 'react';
 import './Menu.css'
-import NavBar from '../NavBar'
-import TableInfo from '../TableInfo'
+import NavBar from "../NavBar";
+import TableInfo from "../TableInfo";
 
 const Index = () => {
-  return (
-    <div className='parent'>
-      <div className='firstChild'>
-        <NavBar/>
-      </div>
-      <div className='secondChild'>
-        <TableInfo/>
-      </div>
-    </div>
-  )
-}
+    return (
+        <div className='parent'>
+            <div className='firstChild'>
+                <NavBar/>
+            </div>
+            <div className='secondChild'>
+            <TableInfo/>
+            {/* merge test */}
+            </div>
+        </div>
+    );
+};
 
-export default Index
+export default Index;
